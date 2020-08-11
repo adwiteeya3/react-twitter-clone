8731b86adb0d3ae59b224fd4a517a55485d83931
Adwiteeya <adwiteeya3@gmail.com>
2020-08-11 18:53:40 +0530

    sidebar done

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -16,7 +16,7 @@ function Sidebar() {
     return (
         <div className='sidebar'>
             {/* Twitter icon */}
-            <TwitterIcon />
+            <TwitterIcon className="sidebar_twitterIcon"/>
             {/* SidebarOption */}
             <SidebarOption active Icon={HomeIcon} text="Home"/>
             <SidebarOption Icon={SearchIcon} text="Explore" />
