f9924647da14bafb9c8a4c9783f26c6dbbe77d74
Adwiteeya <adwiteeya3@gmail.com>
2020-08-11 18:35:51 +0530

    sidebar completed

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -10,6 +10,7 @@ import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
 import ListAltIcon from '@material-ui/icons/ListAlt';
 import PermIdentityIcon from '@material-ui/icons/PermIdentity';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+import { Button } from "@material-ui/core";
 
 function Sidebar() {
     return (
@@ -17,7 +18,7 @@ function Sidebar() {
             {/* Twitter icon */}
             <TwitterIcon />
             {/* SidebarOption */}
-            <SidebarOption Icon={HomeIcon} text="Home"/>
+            <SidebarOption active Icon={HomeIcon} text="Home"/>
             <SidebarOption Icon={SearchIcon} text="Explore" />
             <SidebarOption Icon={NotificationsNoneIcon} text="Notifications"/>
             <SidebarOption Icon={MailOutlineIcon} text="Messages"/>
@@ -27,6 +28,7 @@ function Sidebar() {
             <SidebarOption Icon={MoreHorizIcon} text="More"/>
 
             {/* Button -> Tweet */}
+            <Button variant="outlined" className="sidebar_tweet" fullWidth>Tweet</Button>
             
         </div>
     );
--- src/SidebarOption.js
+++ src/SidebarOption.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './SidebarOption.css';
 
-function SidebarOption({ text, Icon }) {
+function SidebarOption({ active, text, Icon }) {
     return (
-        <div className='sidebarOption'>
+        <div className={`sidebarOption ${active&& 'sidebarOption--active'}`}>
             <Icon />
             <h2>{text}</h2>
         </div>
