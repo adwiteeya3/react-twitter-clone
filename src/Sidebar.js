de6232fe934e8bccfc4542482a3dd35adda9da86
Adwiteeya <adwiteeya3@gmail.com>
2020-08-11 17:43:49 +0530

    sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from 'react';
+import './Sidebar.css';
 
 function Sidebar() {
     return (
-        <div>
+        <div className="sidebar">
+            {/* Twitter icon */}
+
+            {/* SidebarOption */}
+            {/* SidebarOption */}
+            {/* SidebarOption */}
+            {/* SidebarOption */}
+
+            {/* Button -> Tweet */}
             
         </div>
-    )
+    );
 }
 
-export default Sidebar
+export default Sidebar;
