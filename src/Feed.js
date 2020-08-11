ea1b6e738b05ae4ed9633fc1fdbdb4dae079a160
Adwiteeya <adwiteeya3@gmail.com>
2020-08-11 21:37:22 +0530

    feed formatting

--- src/Feed.js
+++ src/Feed.js
@@ -3,15 +3,58 @@ import "./Feed.css";
 
 function Feed() {
     return (
-        <div> 
+        <div className="feed"> 
            {/* Header */}
-           <div className="feed">  
+           <div className="feed_header">  
                 <h2>Home</h2>
            </div> 
             
            {/* TweetBox */}
 
            {/* Post */}
+           <h5>post</h5>
+           <h5>post</h5>
+           <h5>post</h5>
+           <h5>post</h5>
+           <h5>post</h5>
+           <h5>post</h5>
+           <h5>post</h5>
+           <h5>post</h5>
+           <h5>post</h5>
+           <h5>post</h5>
+           <h5>post</h5>
+           <h5>post</h5>
+           <h5>post</h5>
+           <h5>post</h5>
+           <h5>post</h5>
+           <h5>post</h5>
+           <h5>post</h5>
+           <h5>post</h5>
+           <h5>post</h5>
+           <h5>post</h5>
+           <h5>post</h5>
+           <h5>post</h5>
+           <h5>post</h5>
+           <h5>post</h5>
+           <h5>post</h5>
+           <h5>post</h5>
+           <h5>post</h5>
+           <h5>post</h5>
+           <h5>post</h5>
+           <h5>post</h5>
+           <h5>post</h5>
+           <h5>post</h5>
+           <h5>post</h5>
+           <h5>post</h5>
+           <h5>post</h5>
+           <h5>post</h5>
+           <h5>post</h5>
+           <h5>post</h5>
+           <h5>post</h5>
+           <h5>post</h5>
+           <h5>post</h5>
+           <h5>post</h5>
+           
            {/* Post */}
            {/* Post */}
            {/* Post */} 
