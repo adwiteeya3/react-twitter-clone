b26ddcb5280cb9bf1972e9281f5127bed0d1c291
Adwiteeya <adwiteeya3@gmail.com>
2020-08-11 22:31:38 +0530

    update

--- src/TweetBox.js
+++ src/TweetBox.js
@@ -7,11 +7,17 @@ function TweetBox() {
         <div className='tweetBox'>
             <form>
                 <div className='tweetBox_input'>
-                <Avatar src='https://images.unsplash.com/photo-1596921580631-c491e33e2e37?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=4350&q=80' />
-                <input placeholder="What's happening?" type='text'></input>
-                {/* <input placeholder="Enter image URL" type='text'></input> */}
+                    <Avatar src='https://images.unsplash.com/photo-1596921580631-c491e33e2e37?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=4350&q=80' />
+                    <input placeholder="What's happening?" type='text'></input>
                 </div>
-                <Button>Tweet</Button>
+
+                <input
+                className='tweetBox_imageInput' 
+                placeholder="Optional: Enter image URL" 
+                type='text' 
+                />
+
+                <Button className='tweetBox_tweetButton'>Tweet</Button>
             </form>
         </div>
     )
