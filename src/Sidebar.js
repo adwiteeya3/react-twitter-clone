760219754b0309c4d00a2555d2215403ed301fb0
Adwiteeya <adwiteeya3@gmail.com>
2020-08-11 18:22:56 +0530

    sidebar formatting

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -5,6 +5,11 @@ import HomeIcon from '@material-ui/icons/Home';
 import SidebarOption from './SidebarOption';
 import SearchIcon from '@material-ui/icons/Search';
 import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone';
+import MailOutlineIcon from '@material-ui/icons/MailOutline';
+import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
+import ListAltIcon from '@material-ui/icons/ListAlt';
+import PermIdentityIcon from '@material-ui/icons/PermIdentity';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 
 function Sidebar() {
     return (
@@ -15,9 +20,11 @@ function Sidebar() {
             <SidebarOption Icon={HomeIcon} text="Home"/>
             <SidebarOption Icon={SearchIcon} text="Explore" />
             <SidebarOption Icon={NotificationsNoneIcon} text="Notifications"/>
-            {/* SidebarOption */}
-            {/* SidebarOption */}
-            {/* SidebarOption */}
+            <SidebarOption Icon={MailOutlineIcon} text="Messages"/>
+            <SidebarOption Icon={BookmarkBorderIcon} text="Bookmarks"/>
+            <SidebarOption Icon={ListAltIcon} text="Lists"/>
+            <SidebarOption Icon={PermIdentityIcon} text="Profile"/>
+            <SidebarOption Icon={MoreHorizIcon} text="More"/>
 
             {/* Button -> Tweet */}
             
