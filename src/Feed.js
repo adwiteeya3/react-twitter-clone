fe13765a839c3da8b54c3ecc7ce0ea12ece27d49
Adwiteeya <adwiteeya3@gmail.com>
2020-08-13 21:50:15 +0530

    database

--- src/Feed.js
+++ src/Feed.js
@@ -15,10 +15,15 @@ function Feed() {
             <TweetBox />
 
            {/* Post */}
-           <Post />
-           {/* Post */}
-           {/* Post */}
-           {/* Post */} 
+           <Post 
+             displayName="Adwiteeya"
+             username="adwiteeya6"
+             verified= {true}
+             text="Whola!" 
+             avatar= "https://images.unsplash.com/photo-1597011769896-d355e2afcbab?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2600&q=80"
+             image= "https://player.vimeo.com/video/288484037?title=0&portrait=0&byline=0&autoplay=1"
+            />
+           
 
         </div>
     );
